107103a2ddb11eb99cbbd9d986db828c2ed0cc7f
Laraib007 <lrbkhan01@gmail.com>
2024-06-08 22:29:10 -1200

    put

---
+++ app.js
@@ -0,0 +1,63 @@
+// let x = 5;
+// x = 6+1
+// console.log(x)
+
+// function work (){
+//     console.log("working1")
+// }
+
+// const work2 = (a, b) =>  a + b
+// work()
+// console.log(work2(2,3))
+// alert("working")
+// const events = [1,3,7,11]
+// // var lock = events.map(function(val){
+// //         return val -1
+// // })
+
+// var lock = events.map(val => val - 1)
+// console.log(lock)
+
+// const events = [1,2,3,4,5,6,7]
+
+// const work = events.filter(val => val % 2 !== 0)
+
+// console.log(work)
+
+// work.forEach(val => console.log( val))
+
+
+// function work  (x, y = 2, z = 26) {
+// return x = 1 + y + z;
+// }
+//  ok = work()
+// console.log(ok)
+
+// function para (x, y, ...a){
+// let answer = 0;
+// a.forEach(val => {
+//     answer += val
+// })
+// return x + y + answer
+// }
+// console.log(para(1,2,3,4,5,6,9))
+
+
+// let para = [1, "hello", 3, "world"]
+// let para2 = ["working", 5, 9, "rule", ...para]
+
+// console.log(para2)
+
+// let val = 5
+// // console.log(`This is number ${5}`)
+// console.log(val === 5 ? true : false)
+
+// let obj = {
+//     square: (num) => num ** 2
+// }
+// console.log(obj.square(5))
+// let objkey = "key"
+// console.log(obj[objkey])
+
+
+
