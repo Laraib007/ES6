a70bf8afcf01bd277bf71ee8e9c126b9ab6d647d
Laraib007 <lrbkhan01@gmail.com>
2024-06-11 19:50:28 -1200

    promises appendChild

--- app.js
+++ app.js
@@ -331,25 +331,25 @@
 //     console.log("Not found")
 // })
 
-const loadScript = (src)=>{
-    return new Promise((resolve, reject)=>{
-         let script = document.createElement("script")
-         script.type = "text/javascript"
-         script.src = src
-         document.body.appendChild(script)
-         script.onload=()=>{
-            resolve(1)
-         }
-          script.onerror=()=>{reject(0)}
-    })
-}
+// const loadScript = (src)=>{
+//     return new Promise((resolve, reject)=>{
+//          let script = document.createElement("script")
+//          script.type = "text/javascript"
+//          script.src = src
+//          document.body.appendChild(script)
+//          script.onload=()=>{
+//             resolve(1)
+//          }
+//           script.onerror=()=>{reject(0)}
+//     })
+// }
 
-let p1 = loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js")
-p1.then(()=>{
-    console.log("Script Worked")
-}).catch(()=>{
-    console.log("Script Not Found")
-})
+// let p1 = loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js")
+// p1.then(()=>{
+//     console.log("Script Worked")
+// }).catch(()=>{
+//     console.log("Script Not Found")
+// })
 
 
 
