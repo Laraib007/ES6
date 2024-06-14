e43b75701d0693d7d6e22facd0d15b811bdf01d6
Laraib007 <lrbkhan01@gmail.com>
2024-06-15 00:19:42 +0500

    Project with rest API with all product print
    using forLoop in rest api

--- app.js
+++ app.js
@@ -587,6 +587,34 @@
 
 // localStorage.setItem("as", "as")
 
+// let url = "https://fakestoreapiserver.reactbd.com/nextamazon"
+
+// let response = fetch(url)
+//     response.then((v)=>{
+//             return v.json()
+//     }
+// ).then((v)=>{
+//     ihtml = ""
+//     for(item in v){
+//     console.log(v[item])
+    
+//     ihtml += `
+//        <div class="card my-4 mx-4" style="width: 18rem;">
+//         <img src="${v[item].image}" class="card-img-top my-2 d-block w-80 h-50" alt="...">
+//         <div class="card-body">
+//           <h5 class="card-title">${v[item].title}</h5>
+//           <p>Price: $${v[item].price}</p>
+//           <p>category: ${v[item].category}</p>
+//           <p>quantity avalabile: ${v[item].quantity}</p>
+//           <a href="#" class="btn btn-primary">Buy Now</a>
+//         </div>
+//       </div>
+//     `
+//     Cards.innerHTML = ihtml
+//     }
+// })
+
+
 let url = "https://fakestoreapiserver.reactbd.com/nextamazon"
 
 let response = fetch(url)
