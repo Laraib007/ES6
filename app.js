99789bd8581d72ef0107ed24c328663b0818fb46
Laraib007 <lrbkhan01@gmail.com>
2024-06-15 12:40:08 +0500

    using for loop o array

--- app.js
+++ app.js
@@ -615,49 +615,72 @@
 // })
 
 
-let url = "https://fakestoreapiserver.reactbd.com/nextamazon"
+// let url = "https://fakestoreapiserver.reactbd.com/nextamazon"
 
-let response = fetch(url)
-    response.then((v)=>{
-            return v.json()
-    }
-).then((v)=>{
-    ihtml = ""
-    for(item in v){
-    console.log(v[item])
+// let response = fetch(url)
+//     response.then((v)=>{
+//             return v.json()
+//     }
+// ).then((v)=>{
+//     ihtml = ""
+//     for(item in v){
+//     console.log(v[item])
     
-    ihtml += `
-       <div class="card my-4 mx-4" style="width: 18rem;">
-        <img src="${v[item].image}" class="card-img-top my-2 d-block w-80 h-50" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${v[item].title}</h5>
-          <p>Price: $${v[item].price}</p>
-          <p>category: ${v[item].category}</p>
-          <p>quantity avalabile: ${v[item].quantity}</p>
-          <a href="#" class="btn btn-primary">Buy Now</a>
-        </div>
-      </div>
-    `
-    Cards.innerHTML = ihtml
-    }
-})
+//     ihtml += `
+//        <div class="card my-4 mx-4" style="width: 18rem;">
+//         <img src="${v[item].image}" class="card-img-top my-2 d-block w-80 h-50" alt="...">
+//         <div class="card-body">
+//           <h5 class="card-title">${v[item].title}</h5>
+//           <p>Price: $${v[item].price}</p>
+//           <p>category: ${v[item].category}</p>
+//           <p>quantity avalabile: ${v[item].quantity}</p>
+//           <a href="#" class="btn btn-primary">Buy Now</a>
+//         </div>
+//       </div>
+//     `
+//     Cards.innerHTML = ihtml
+//     }
+// })
 
+// let a = ["Labi","Khan", "Working"]
 
+// let sleep = async(seconds)=>{
+//   return new Promise((resolve, reject)=>{
+//       setTimeout(()=>{ resolve(true)}, seconds * 1000)
+//   })
+// }
+// let hackMan = async(message)=>{
+//       await sleep(2)
+//       console.log(message)
+// }
 
+// let work = async()=>{
+//   for (let i = 0; i < a.length; i++)
+//   {
+//    await hackMan(a[i])
+//   }
+// }
 
+// work()
 
+// let a = ["labi", "LRB", "Khan"]
 
+// let sleep = async(seconds) =>{
+//   return new Promise((resolve, reject)=>{
+//       setTimeout(()=>{resolve(true)}, seconds * 1000)
+//   })
+// }
 
+// let hackMan = async(message)=>{
+//     await sleep(2)
+//     console.log(message)
+// }
 
-
-
-
-
-
-
-
-
-
+// (async()=>{
+//   for (let i = 0; i < a.length; i++){
+//      await hackMan(a[i])
+//   }
+// })()
 
 
 
