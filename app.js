07c0964459ed8776e341e7c8206d2ea36b73e63d
Laraib007 <lrbkhan01@gmail.com>
2024-06-19 12:21:34 +0500

    more properties of document

--- app.js
+++ app.js
@@ -1001,8 +1001,12 @@ let a = document.getElementsByTagName("div")[0]
 
 let div = document.createElement("div")
 div.innerHTML = "<h1>HELLO WORLD</h1>"
-a.appendChild(div)
-
+// a.appendChild(div)
+// a.append(div)
+// a.before(div)
+// a.prepend(div)
+// a.after(div)
+// a.replaceWith(div)
 
 
 
