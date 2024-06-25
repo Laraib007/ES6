0b13b3ddb7dea599cdc170a844c9e8b6ded5a6e2
Laraib007 <lrbkhan01@gmail.com>
2024-06-25 22:45:17 +0500

    installing nodemon in express js

--- app.js
+++ app.js
@@ -1208,15 +1208,18 @@
 // ahello("Labi")
 
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello Wania!')
+    console.log(req.query.name)
+  res.send('Hello opkk!')
 })
 
 app.get('/about', (req, res) => {
-    res.send('About')
+    res.sendFile(path.join(__dirname, "index.html"))
+    // res.send('About')
   })
 
   app.get('/home', (req, res) => {
